d6f238d4881c32b6cff585ea4a5b5206674c182d
Remi Mercier <remi.merci@gmail.com>
2018-05-17 14:56:54 +0200

    Handle cocktails logic throughout app

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,5 +1,5 @@
 class CocktailsController < ApplicationController
-  before_action :set_cocktail, only: [ :show, :create ]
+  before_action :set_cocktail, only: [ :show ]
   def index
     @cocktails = Cocktail.all
   end
