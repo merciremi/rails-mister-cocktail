0960356a977c4770a2c94a66e257da1dc6d792b7
Remi Mercier <remi.merci@gmail.com>
2018-05-17 12:47:10 +0200

    Reorganize code logic

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,10 +4,10 @@ require 'open-uri'
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 drinks_serialized = open(url).read
 drinks = JSON.parse(drinks_serialized)
+drinks_array = drinks['drinks']
 
 puts 'Creating 100 ingredients...'
 
-drinks_array = drinks['drinks']
 drinks_array.each do |drink|
   ingredient = Ingredient.new(
     name: drink["strIngredient1"]
