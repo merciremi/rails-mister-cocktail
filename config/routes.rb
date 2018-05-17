78340edf07f5b4c30713ef2b6460204eb451369e
Remi Mercier <remi.merci@gmail.com>
2018-05-17 15:20:50 +0200

    Add routes for dose model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'doses/new'
+  get 'doses/create'
+  get 'doses/destroy'
   root 'cocktails#index'
-  resources :cocktails, only: [ :index, :show, :new, :create ]
+  resources :cocktails, only: [ :index, :show, :new, :create ] do
+    resources :doses, only: [ :new, :create, :destroy ]
+  end
 end
